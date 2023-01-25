27b461813bcfd4492f60ae7b46663b511d3a140c
felipecuervo500 <123320223+felipecuervo500@users.noreply.github.com>
2023-01-25 09:27:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import operaciones
+
+def main():
+    resu = operaciones.suma(15, 7)
+    resurest = operaciones.resta(20, 11)
+    resumulti = operaciones.multiplicacion(6, 8)
+    resudivi = operaciones.division(30, 7)
+    print(resu, resurest, resumulti, resudivi)
+
+if __name__ == '__main__':
+    main()
